920e78e943abf27c34e7cc8b62aedb3857fef825
jel-lambda <sheee724@gmail.com>
2022-07-14 14:51:56 +0900

    chore: make data name list on text file

--- generate_name.py
+++ generate_name.py
@@ -1,12 +1,12 @@
 import os.path
 
-file_path = r"/home/cvmlserver/Seohyeon/TransWeather/data/test/input"
+file_path = r"/home/cvmlserver/Seohyeon/TransWeather/data/train/Amaro/gt"
 
 files = os.listdir(file_path)
 
 for name in files:
     src = os.path.join(file_path, name)
-    new_name =  name.split('_')[0] + '_1977.jpg'
+    new_name =  name.split('_')[0] + '_Amaro.jpg'
     dst = os.path.join(file_path, new_name)
-    print("input/"+ new_name)
+    print("Amaro/input/"+ new_name)
     os.rename(src, dst)
