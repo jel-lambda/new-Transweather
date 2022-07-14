8da104201d9188f6d570a7d498b463e9c7f5bb42
jel-lambda <sheee724@gmail.com>
2022-07-14 15:01:56 +0900

    feat: add function just for renaming

--- generate_name.py
+++ generate_name.py
@@ -10,6 +10,13 @@ def rename_files(file_path, filter_name):
         dst = os.path.join(file_path, new_name)
         os.rename(src, dst)
 
+def make_textfile(file_path, filter_name):
+    f = open("/home/cvmlserver/Seohyeon/TransWeather/data/train/allfilter.txt", 'a')
+    files = os.listdir(file_path)
+    for name in files:
+        f.write(filter_name + "/input/"+ name + '\n')
+    f.close
+
 def rename_and_textfile(file_path, filter_name):
     f = open("/home/cvmlserver/Seohyeon/TransWeather/data/train/allfilter.txt", 'a')
     files = os.listdir(file_path)
@@ -17,7 +24,7 @@ def rename_and_textfile(file_path, filter_name):
         src = os.path.join(file_path, name)
         new_name =  name.split('_')[0] + '_' + filter_name +'.jpg'
         dst = os.path.join(file_path, new_name)
-        f.write(filter_name + "/input/"+ name + '\n')
+        f.write(filter_name + "/input/"+ new_name + '\n')
         os.rename(src, dst)
     f.close
 
@@ -34,11 +41,14 @@ def in_all_dir(dir_path,func):
         for directory in dir_list:
             if directory.split('.')[-1] != 'txt':
                 file_path =  os.path.join(dir_path, directory + '/gt/')
-                rename_and_textfile(file_path, directory)
+                if func == "make_textfile":
+                    make_textfile(file_path,directory)
+                else:
+                    rename_and_textfile(file_path, directory)
 
 def main(): 
-    dir_path = r"/home/cvmlserver/Seohyeon/TransWeather/data/test/"
-    in_all_dir(dir_path, 'rename_files')
+    dir_path = r"/home/cvmlserver/Seohyeon/TransWeather/data/train/"
+    in_all_dir(dir_path, 'make_textfile')
 
 if(__name__) == '__main__':
     main()
